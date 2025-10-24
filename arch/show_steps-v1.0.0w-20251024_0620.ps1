897b5c2e13788dce6710b4ede3b2cb148a73dc0b
natrajv <natrajv@yahoo.com>
2025-10-24 06:43:28 +0530

    1st commit

---
+++ arch/show_steps-v1.0.0w-20251024_0620.ps1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+    Initializes a new uv project with Jupyter kernel, OpenPyXL for Excel and standard packages setup.
+.DESCRIPTION
+    This script creates a new directory, initializes a uv project, sets up a Jupyter kernel
+.PARAMETER SnipDir
+    The name of the directory to create and initialize the uv project in.
+   #>
+param(
+    [string]$StepsID
+)
+#*--Validate Param--*
+if ($PSBoundParameters.ContainsKey('StepsID')) {
+	Write-Host "✅ Name was passed: $StepsID" -ForegroundColor Green
+} else {
+	Write-Host "❌ Name was NOT passed" -ForegroundColor Red
+}
+
+<#**==Index-Functions==**
+@01. show_steps
+@02. ...
+@03. ...
+#>
+
+#**==Functions==*
+function show_steps {
+    switch ($StepsID) {
+        "git01" {
+             # Display steps of git
+            Write-Host "*--Steps of Git Push--*" -ForegroundColor Red
+
+            $steps_git_push = @"
+            @01. git status
+            @02. git add .
+            @03. git status
+            @04. git git commit -m "get_df_by_range(…) added"
+            @05. git branch #Check current active branch
+            @06. git remote -v #Check remote 
+            @07. #git push -u origin main #Push to remote one-time
+            @08. git push #Push to remote subsequent times
+            "@
+
+            Write-Host $steps_git_push -ForegroundColor Green
+}        }
+        default {
+            Write-Host "❌ Steps ID:  $StepsID not avilable" -ForegroundColor Red
+        }
+    }
---
+++ readme!.txt
@@ -0,0 +1,21 @@
+*--AVN PowerShell scripts--*
+*--
+* Frequently used PowerShell scripts, display custom information such as git steps
+*-----*
+
+**==Index==**
+@01. show_steps_git_push() 
+@02. (...)
+
+**==Details==**
+@01. show_steps_git_push()
+
+*=====*
+@02. (...)
+
+**==Version==**
+v1.0.0-20251024_0603
+v1.0.0w-20251024_0603
+---
+CTD: 20251024_0603
+LUD: 20251024_0604
